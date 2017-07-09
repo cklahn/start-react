4a557fefdb5cc5116029111fe81502348674f483
Claudia Klahn <csklahn@gmail.com>
2017-07-09 21:20:26 +0200

    set properties on components, lesson 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // state component
 class App extends React.Component {
   render() {
+    let txt = this.props.txt;
+
     return (
       <div>
-          <h1>class component (state component)</h1>
-          <bold>Bold</bold>
+          <h1>{this.props.txt}</h1>
+          <bold>{txt}</bold>
       </div>
     )
   }
 }
+
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "This is the default text"
+}
+
 
 // stateless function component
 // const App = () => <h1>stateless function component</h1>
