16579f3111e3c4bc094b19e996ea76dec9513fec
Claudia Klahn <csklahn@gmail.com>
2017-08-12 13:05:16 +0200

    use React ref to get a reference to specific components, lesson 10

--- src/App.js
+++ src/App.js
@@ -1,39 +1,68 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // state component
 class App extends React.Component
 {
   constructor() {
     super();
-    this.state = {currentEvent: '---'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
 
-  update(e) {
-    this.setState({currentEvent: e.type})
+  update() {
+    this.setState({
+      a: this.a.value,
+      b: this.refs.b.value,
+      c: ReactDOM.findDOMNode(this.c).value,
+      d: this.d.refs.input.value
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          onFocus={this.update}
-          onBlur={this.update}
-          onDoubleClick={this.update}
-          onTouchStart={this.update}
-          onTouchMove={this.update}
-          onTouchEnd={this.update}
-          cols="30"
-          rows="10" />
-        <h1>{this.state.currentEvent}</h1>
+        <input
+          ref={node => this.a = node}
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.a}
+        <hr />
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.b}
+        <hr />
+        <Input
+          ref={component => this.c = component}
+          update={this.update.bind(this)}
+        /> {this.state.c}
+        <hr />
+        <Input2
+          ref={component => this.d = component}
+          update={this.update.bind(this)}
+        /> {this.state.d}
       </div>
     )
   }
 }
 
+class Input extends React.Component {
+  render() {
+    return (
+        <input type="text" onChange={this.props.update} />
+    )
+  }
+}
+
+class Input2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="input" type="text" onChange={this.props.update} />
+      </div>
+    )
+  }
+}
 
 export default App
